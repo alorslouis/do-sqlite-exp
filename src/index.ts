084436e2fa025552727fc62d4e62e8012f028eec
alorslouis <alorslouis@users.noreply.github.com>
2024-10-13 11:03:15 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { DurableObject } from "cloudflare:workers";
+import { z } from "zod"
 
 /**
  * Welcome to Cloudflare Workers! This is your first Durable Objects application.
@@ -38,6 +39,133 @@ export class MyDurableObject extends DurableObject {
 	}
 }
 
+const seatId = z.string()
+
+type SeatId = z.infer<typeof seatId>
+
+const occupant = z.string().nullish()
+
+const seatList = z.object({
+	seatId,
+	occupant
+})
+
+type SeatList = z.infer<typeof seatList>
+
+function charToNumber(char: string) {
+	return char.toLowerCase().charCodeAt(0) - 96;
+}
+
+// Function to convert a number back to a character
+function numberToChar(num: number) {
+	return String.fromCharCode(num + 96);
+}
+
+export class SqliteTestClass extends DurableObject {
+
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+	}
+
+	sql = this.ctx.storage.sql;
+
+	createDummyFlight() {
+		const flightOccupancy = Math.round(Math.random() * 100)
+		const perRow = 6
+		const cols = 2
+
+		const totalRows = flightOccupancy / perRow
+
+		let seatListHolder: SeatList[][] = []
+
+		for (let i = 0; i <= totalRows; i++) {
+
+			const rowNum = i + 1
+
+			const seats = Array.from({ length: perRow }).map((_, i) => {
+				const seat: SeatList = {
+					occupant: null,
+					seatId: `${rowNum}${numberToChar(i + 1).toUpperCase()}`
+				}
+				return seat
+			})
+
+			seatListHolder.push(seats)
+
+		}
+
+		return seatListHolder.flat()
+
+
+	}
+	// Application calls this when the flight is first created to set up the seat map.
+	initializeFlight(seatList: SeatList[]) {
+		this.sql.exec(`
+      CREATE TABLE IF NOT EXISTS seats (
+        seatId TEXT PRIMARY KEY,  -- e.g. "3B"
+        occupant TEXT             -- null if available
+      )
+    `);
+
+		for (let seat of seatList) {
+			this.sql.exec(`INSERT INTO seats VALUES (?, null)`, seat.seatId);
+		}
+	}
+
+	// Get a list of available seats.
+	getAvailable() {
+		// NOTE: just seaId here, as we're querying for `IS NULL`
+		let results: SeatId[] = [];
+
+		// Query returns a cursor.
+		let cursor = this.sql.exec(`SELECT seatId FROM seats WHERE occupant IS NULL`);
+
+		console.log({ cursor })
+
+		// Cursors are iterable.
+		for (let row of cursor) {
+			// Each row is an object with a property for each column.
+			const rowGot = Object.values(row)
+			const tryParse = seatId.safeParse(rowGot[0])
+
+			if (tryParse.success) {
+				results.push(tryParse.data);
+			}
+		}
+
+		return results;
+	}
+
+	// Assign passenger to a seat.
+	assignSeat(seatInfo: SeatList) {
+		const { seatId, occupant } = seatInfo
+		//
+		// Check that seat isn't occupied.
+		let cursor = this.sql.exec(`SELECT occupant FROM seats WHERE seatId = ?`, seatId);
+		let result = [...cursor][0];  // Get the first result from the cursor.
+		if (!result) {
+			throw new Error("No such seat: " + seatId);
+		}
+		if (result.occupant !== null) {
+			throw new Error("Seat is occupied: " + seatId);
+		}
+
+		// If the occupant is already in a different seat, remove them.
+		this.sql.exec(`UPDATE seats SET occupant = null WHERE occupant = ?`, occupant);
+
+		// Assign the seat. Note: We don't have to worry that a concurrent request may
+		// have grabbed the seat between the two queries, because the code is synchronous
+		// (no `await`s) and the database is private to this Durable Object. Nothing else
+		// could have changed since we checked that the seat was available earlier!
+		this.sql.exec(`UPDATE seats SET occupant = ? WHERE seatId = ?`, occupant, seatId);
+	}
+}
+
+
+const methods = z.enum(["available", "assign"])
+const planeId = z.string()
+
+
 export default {
 	/**
 	 * This is the standard fetch handler for a Cloudflare Worker
@@ -50,7 +178,53 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		// We will create a `DurableObjectId` using the pathname from the Worker request
 		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
+
+
+		const urlPath = new URL(request.url).pathname
+
+		if (urlPath.includes("seats")) {
+
+			const room = urlPath.split("/").filter(x => x.length)
+
+			if (!room.length) throw new Error("ffs")
+
+
+			if (room.length !== 3) throw new Error("wrong methods")
+
+
+			const method = methods.parse(room[1])
+
+			console.log({ method })
+
+			const roomId = room[room.length - 1]
+
+			if (roomId === "seats") {
+				throw new Error("incorrect!")
+			}
+
+			let id: DurableObjectId = env.SQLITE_TEST.idFromName(roomId);
+
+			let planeStub = env.SQLITE_TEST.get(id);
+
+
+
+			try {
+				const availableRows = await planeStub.getAvailable()
+				return Response.json(availableRows)
+
+			} catch (error) {
+				console.error(error)
+				console.info(`failed to get table for: ${id}\nrunning init`)
+
+				const totalRows = await planeStub.createDummyFlight()
+				await planeStub.initializeFlight(totalRows)
+				const availableRows = await planeStub.getAvailable()
+				return Response.json(availableRows)
+			}
+
+		}
+
+		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(urlPath);
 
 		// This stub creates a communication channel with the Durable Object instance
 		// The Durable Object constructor will be invoked upon the first call for a given id
--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -2,4 +2,5 @@
 
 interface Env {
 	MY_DURABLE_OBJECT: DurableObjectNamespace<import("./src/index").MyDurableObject>;
+	SQLITE_TEST: DurableObjectNamespace<import("./src/index").SqliteTestClass>;
 }
